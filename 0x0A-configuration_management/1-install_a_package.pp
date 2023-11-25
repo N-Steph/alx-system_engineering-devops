f3e2d5f0b82cfb22e121e75a475d08619d6df271
N-Steph <snouaffo@gmail.com>
2023-11-25 20:42:16 +0000

    added an exec resource in order to install werkzeug version compatible with the installed flask version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,12 @@
 
 $package = 'flask==2.1.0'
 
+exec { 'pip3 install werkzeug==2.1.1':
+command => '/usr/bin/pip3 install werkzeug==2.1.1'
+}
+
 package { $package :
 ensure   => 'installed',
-provider => 'pip3'
+provider => 'pip3',
+require  => Exec['pip3 install werkzeug==2.1.1']
 }
