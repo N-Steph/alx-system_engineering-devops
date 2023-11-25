148c9aad016fd2185e10c53976c0cdc23d152dd7
N-Steph <snouaffo@gmail.com>
2023-11-25 20:14:11 +0000

    used the package resource to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Install flask version 2.1.0 from pip3
 
-exec { 'pip3 install flask==2.1.0':
-command => '/usr/bin/pip3 install flask==2.1.0'
+$package = 'flask==2.1.0'
+
+package { $package :
+ensure   => 'installed',
+provider => 'pip3'
 }
