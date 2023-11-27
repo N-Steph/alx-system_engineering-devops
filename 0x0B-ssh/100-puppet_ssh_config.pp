38b0b891494072034479b53b9270858a19baf988
N-Steph <snouaffo@gmail.com>
2023-11-27 04:17:13 +0000

    used puppet to set up client SSH configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+$path_to_file = '/root/.ssh/config'
+$str = "Host\n\tHostName ubuntu\n\tUser ubuntui\n\tPort 22\n\tIdentityFile ~/.ssh/school"
+
+file { $path_to_file :
+ensure  => 'present',
+path    => $path_to_file,
+content => $str
+}
