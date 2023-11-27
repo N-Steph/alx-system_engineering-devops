fa4950ae890aadedb0943836f7983c53c70ad71a
N-Steph <snouaffo@gmail.com>
2023-11-27 04:20:22 +0000

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # sets up client SSH configuration file
 
-$path_to_file = '/root/.ssh/config'
+$path_to_file = '/etc/ssh/ssh_config'
 $str = "Host\n\tHostName ubuntu\n\tUser ubuntui\n\tPort 22\n\tIdentityFile ~/.ssh/school"
 
 file { $path_to_file :
