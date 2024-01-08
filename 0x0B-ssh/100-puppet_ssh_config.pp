4e73addef449924bdbfae185a8755912a540b79b
N-Steph <119834045+N-Steph@users.noreply.github.com>
2024-01-08 11:23:33 +0100

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# sets up client SSH configuration file
-
-$path_to_file = '/root/.ssh/config' 
-$str = "IdentityFile ~/.ssh/school"
-
-file { $path_to_file:
-ensure  => 'present',
-content => $str
-}
--- 0x0B-ssh/README.md
+++
@@ -1,2 +0,0 @@
-## Description
-In this directory we are going to explore how to create and ssh key pair and how to connect to a remote server using ssh
