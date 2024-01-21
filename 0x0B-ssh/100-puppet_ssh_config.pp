7981162982b2854c8f853cf302e27b3c80f2cb42
N-Steph <snouaffo@gmail.com>
2024-01-21 02:07:27 +0000

    added an option to the sed command

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Conigures the client SSH configuration file to use the private key ~/.ssh/school
 
-$config_content = '/usr/bin/sed "s/#   IdentityFile ~\/.ssh\/id_rsa/IdentityFile ~\/.ssh\/school/" /etc/ssh/ssh_config > /etc/ssh/ssh_config'
+$config_content = '/usr/bin/sed -i "s/#   IdentityFile ~\/.ssh\/id_rsa/IdentityFile ~\/.ssh\/school/" /etc/ssh/ssh_config'
 
 exec {$config_content:
   command => $config_content
