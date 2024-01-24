d24c4273f92317d3f5c4bc03265cda61292ffe87
N-Steph <snouaffo@gmail.com>
2024-01-24 12:43:48 +0000

    corrected typo and added another resource

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -22,8 +22,13 @@ exec {'redirection and custom_404.html page content':
   command => 'sed -i "s/server_name _;/server_name _;\n\trewrite\
  ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;\
 \n\n\terror_page 404 \/custom_404.html;\n\tlocation = \/custom_404.html \
-{\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}\n/" /etc/nginx/sites-available/default'
-  path    => '/usr/bin',
+  {\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}\n/" /etc/nginx/sites-available/default',
+  path    => '/usr/bin/',
+  require => Package['Install nginx']
+}
+
+exec {'nginx listen on port 80':
+  command =>'/usr/sbin/ufw nginx \'HTTPS\'',
   require => Package['Install nginx']
 }
 
