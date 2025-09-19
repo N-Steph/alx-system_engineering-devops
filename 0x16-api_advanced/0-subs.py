0afa32b24f82dc347ed60af189c10b2a006eb8e4
N-Steph <snouaffo@gmail.com>
2025-09-19 13:20:55 +0100

    pep 8 compliant

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,12 @@
 
 import requests
 
-def number_of_subscribers(subreddit):
-	"""Returns the number of subscribers in a subreddit"""
-	response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
-	if (response.status_code == 200):
-		data = response.json()
-		return data["data"]["subscribers"]
-	else:
-		return 0 
 
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers in a subreddit"""
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    if (response.status_code == 200):
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
