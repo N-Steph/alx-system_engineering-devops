e32ace60e5ca2e5654af56ea383622467a97cf96
N-Steph <snouaffo@gmail.com>
2025-09-22 13:26:51 +0100

    implementation of the recurse function

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Implementation of the recurse function"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """Returns a list of all hot articles of a subreddit"""
+    params = {}
+    headers = {
+        "User-Agent": "recurse"
+    }
+    if (len(hot_list) != 0 and hot_list[-1] == ""):
+        del hot_list[-1]
+        return hot_list
+    elif (len(hot_list) != 0 and hot_list[-1] != ""):
+        params["after"] = hot_list[-1]
+        del hot_list[-1]
+
+    api_endpoint = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(api_endpoint,
+                            params=params,
+                            headers=headers,
+                            allow_redirects=False)
+    print(response.status_code)
+    if (response.status_code == 200):
+        for post in response.json()["data"]["children"]:
+            hot_list.append(post["data"]["title"])
+        hot_list.append(response.json()["data"]["after"])
+        # print(hot_list)
+        print("start next recursion")
+        return recurse(subreddit, hot_list)
+    else:
+        if (len(hot_list) != 0):
+            return hot_list
+        return None
