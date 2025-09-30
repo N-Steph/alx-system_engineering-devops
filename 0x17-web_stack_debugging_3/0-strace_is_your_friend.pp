aa1c7354a4ea2e8e30dcff88f909e14480c618cf
N-Steph <snouaffo@gmail.com>
2025-09-30 13:07:57 +0100

    added description

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Fix php extension wrongly written in an include statement
+
 exec { 'fix_phpp_extension':
   command => "/bin/sed -i 's/\\.phpp/.php/g' /var/www/html/wp-settings.php",
   onlyif  => "/bin/grep -q '\\.phpp' /var/www/html/wp-settings.php",
