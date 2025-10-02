ba9162cb3886cfb0ab0ffab316507edef6bbb4d9
N-Steph <snouaffo@gmail.com>
2025-10-02 12:43:59 +0100

    changed ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,31 +1,12 @@
-# Puppet manifest to modify nginx.conf
+# Puppet manifest to change the max number of open file for nginx
 
-# Uncomment multi_accept on;
-exec { 'uncomment_multi_accept':
-  command => "/bin/sed -i 's/^[[:space:]]*#[[:space:]]*multi_accept on;/    multi_accept on;/' /etc/nginx/nginx.conf",
-  onlyif  => "/bin/grep -q '^[[:space:]]*#[[:space:]]*multi_accept on;' /etc/nginx/nginx.conf",
+file_line { 'nginx_ulimit':
+  path   => '/etc/default/nginx',
+  line   => 'ULIMIT="-n 65535"',
+  match  => '^ULIMIT=',
+  notify => Service['nginx'],
 }
 
-# Add open_file_cache directives to http block
-exec { 'add_open_file_cache':
-  command => "/bin/sed -i '/^http {/a\\     open_file_cache max=5000 inactive=60s;\\n' /etc/nginx/nginx.conf",
-  unless  => "/bin/grep -q 'open_file_cache max=' /etc/nginx/nginx.conf",
-}
-
-exec { 'add_open_file_cache_min_uses':
-  command => "/bin/sed -i '/^http {/a\\        open_file_cache_min_uses 1;\\n' /etc/nginx/nginx.conf",
-  unless  => "/bin/grep -q 'open_file_cache_min_uses' /etc/nginx/nginx.conf",
-}
-
-exec { 'add_open_file_cache_errors':
-  command => "/bin/sed -i '/^http {/a\\        open_file_cache_errors on;' /etc/nginx/nginx.conf",
-  unless  => "/bin/grep -q 'open_file_cache_errors' /etc/nginx/nginx.conf",
-}
-
-# Restart nginx to apply changes
-exec { 'restart_nginx':
-  command     => '/usr/sbin/nginx -t && /usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => [Exec['uncomment_multi_accept'], Exec['add_open_file_cache'],
-  Exec['add_open_file_cache_min_uses'], Exec['add_open_file_cache_errors']],
+service { 'nginx':
+  ensure => running,
 }
